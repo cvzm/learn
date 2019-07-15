2450c0c4f71fed2e10d5b80a29dd133374a6fb25
cv <cvzmcc@163.com>
2019-07-15 10:04:43 +0800

    code

--- src/main/java/Nowcoder/S34.py
+++ src/main/java/Nowcoder/S34.py
@@ -1,7 +1,5 @@
 # -*- coding:utf-8 -*-
-
 """
-
 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
     @href https://www.nowcoder.com/practice/1c82e8cf713b4bbeb2a5b31cf5b0417c
 """
@@ -9,6 +7,9 @@
 class Solution:
 
     def FirstNotRepeatingChar(self, s):
+        if s is None or len(s) == 0:
+            return -1
+
         d = {}  # 字典使用 hash table, 理想查询负载度 O(1), 从而代替list
         for c in s:
             ''' 3. 使用get默认值方法 '''
@@ -25,14 +26,6 @@ class Solution:
             #     d[c] = 0
             # d[c] += 1
 
-        ''' 同值排序的顺序不靠谱，首环境影响， 而且此处也没必要排序。'''
-        # s.index(sorted(d, key=lambda x: d[x])[0])
-
-        ''' python 2/3 的字典排序算法不一致，导致遍历字典时， 不同版本的python无法保证 顺序一致性，从而无法满足第一个元素的条件， 。'''
-        # for i in d:
-        #     if d[i] == 1:
-        #         return s.index(i)
-
         for i in range(len(s)):
             if d[s[i]] == 1:
                 return i
@@ -42,4 +35,7 @@ class Solution:
 s = Solution()
 print(s.FirstNotRepeatingChar("google"))
 print(s.FirstNotRepeatingChar("NXWtnzyoHoBhUJaPauJaAitLWNMlkKwDYbbigdMMaYfkVPhGZcrEwp"))
+print(s.FirstNotRepeatingChar("aa"))
+print(s.FirstNotRepeatingChar(""))
+print(s.FirstNotRepeatingChar(None))
 
